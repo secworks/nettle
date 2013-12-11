c6c867991be66bdcdd9675a05d3d1ae41950a0f5
Joachim Strömbergson <joachim@secworks.se>
2013-12-11 18:51:00 +0100

    Added code to check result and print test info.

--- testsuite/chacha-test.c
+++ testsuite/chacha-test.c
@@ -27,6 +27,22 @@
 #include <stdio.h>
 #include "chacha.h"
 
+#define DEBUG 0
+
+void print_block(uint8_t block[64])
+{
+  uint8_t i;
+
+  for (i = 0 ; i < 64 ; i++) {
+    printf("0x%02x ", block[i]);
+    if (((i + 1) % 8) == 0) {
+      printf("\n");
+    }
+  }
+  printf("\n");
+}
+
+
 void test_chacha(const uint8_t *key, const uint8_t *iv, uint8_t *expected, 
                 uint8_t keylength, uint8_t rounds) {
     uint8_t cipher_data[64] =  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -48,27 +64,52 @@ void test_chacha(const uint8_t *key, const uint8_t *iv, uint8_t *expected,
                                  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     
     struct chacha_ctx cipher_ctx;
+
+    uint8_t errors;
     
     chacha_set_key(&cipher_ctx, keylength, key);
     chacha_set_iv(&cipher_ctx, iv);
     chacha_set_rounds(&cipher_ctx, rounds);
     chacha_crypt(&cipher_ctx, 64, &cipher_result[0], &cipher_data[0]);
+
+    if (DEBUG) {
+        printf("Result after encryption:\n");
+        print_block(cipher_result);
+      }
+
+    errors = 0;
+    for (uint8_t i = 0 ; i < 64 ; i++) {
+      if (cipher_result[i] != expected[i]) {
+        errors++;
+      }
+    }
     
-    // TODO add comparison between excpected and cipher_result
+    if (errors > 0) {
+      printf("Error, expected:\n");
+      print_block(&expected[0]);
+      printf("Got:\n");
+      print_block(cipher_result);
+    }
+    else {
+      printf("Success, result matched expected.\n");
+    }
 }
 
 
 int main(void)
 {
-  printf("Test of chacha nettle implementation.\n");
+  printf("Test of chacha nettle implementation\n");
+  printf("------------------------------------\n");
 
   /* Test vectors from draft-strombergson-chacha-test-vectors */
   
   uint8_t tc1_key[32] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+                         0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
   uint8_t tc1_iv[8]   = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
   uint8_t tc1_expected[64] = {0xe2, 0x8a, 0x5f, 0xa4, 0xa6, 0x7f, 0x8c, 0x5d,
                               0xef, 0xed, 0x3e, 0x6f, 0xb7, 0x30, 0x34, 0x86,
                               0xaa, 0x84, 0x27, 0xd3, 0x14, 0x19, 0xa7, 0x29,
